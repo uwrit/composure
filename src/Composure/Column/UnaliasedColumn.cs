a9df6321f669dfac6acc0b843324beafdb2d3f03
Nicholas J Dobbins <ndobb@uw.edu>
2019-07-29 17:49:56 -0700

    Added UnaliasedColumn

---
+++ src/Composure/Column/UnaliasedColumn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Composure
+{
+    public class UnaliasedColumn : Column
+    {
+        public UnaliasedColumn(IColumn column) : base(column) { }
+
+        public UnaliasedColumn(string name) : base(name) { }
+
+        public UnaliasedColumn(string name, IAliasedSet set) : base(name, set) { }
+
+        public UnaliasedColumn(ExpressedColumn column) : base(column.Name) { }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
